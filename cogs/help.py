e9f258ceaf293217743894a88e29a039d2514348
realstealthninja <realstealthninja@gmail.com>
2021-10-09 14:07:27 +0530

    help fix?

--- cogs/help.py
+++ cogs/help.py
@@ -89,7 +89,7 @@ class Help(commands.Cog):
         embed = discord.Embed(title="⠀")
         embed.add_field(name="⮚ Name ⮘", value=f"`{newcommand.qualified_name}`",inline=False )
         embed.add_field(name-"⮚ Aliases ⮘", value=f"` {newcommand.aliases} `", inline=False)
-        embed.add_field(name="⮚ usage ⮘", value=f"`{newcommand.name + newcommand.signature}`", inline=False)
+        embed.add_field(name="⮚ usage ⮘", value=f"`{newcommand.name} {newcommand.signature}`", inline=False)
         embed.add_field(name="⮚ Description ⮘", value=f"`{newcommand.description}`", inline=False)
         await ctx.send(embed=embed)
 
