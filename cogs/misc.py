315bf606fd6b841ff312dc86287305a9a2620d54
realstealthninja <realstealthninja@gmail.com>
2021-10-12 12:57:22 +0530

    aaaaaaaa

--- cogs/misc.py
+++ cogs/misc.py
@@ -48,7 +48,7 @@ class Misc(commands.Cog):
     @commands.command()
     async def uptime(self, ctx):
 
-        delta_uptime = datetime.utcnow() - self.bot.launch_time
+        delta_uptime = datetime.utcnow() - launch_time
         hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
         minutes, seconds = divmod(remainder, 60)
         days, hours = divmod(hours, 24)
