09b2b2731f5887ab71da8b9dba5ffeb51257cf4a
realstealthninja <realstealthninja@gmail.com>
2021-10-15 20:07:32 +0530

    r

--- run.sh
+++ run.sh
@@ -4,11 +4,15 @@ runstealthybotpy() {
     cd storage/Stealthybot
 
     python main.py
+
+    echo "ran python main.py"
 }
 
 runlavalink(){
     cd storage/Stealthybot
 
     java -jar Lavalink.jar
+
+    echo "ran lava link"
 }
 
