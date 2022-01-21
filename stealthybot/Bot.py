6759cc08aa0b8fbfcf7c5e8c43f9d0db68ee25e8
stealthninja <realstealthninja@gmail.com>
2022-01-21 12:09:35 +0530

    more switching to disnake

--- stealthybot/Bot.py
+++ stealthybot/Bot.py
@@ -1,5 +1,5 @@
-import discord, os, coc, dotenv
-from discord.ext import commands
+import disnake, os, coc, dotenv
+from disnake.ext import commands
 
 
 dotenv.load_dotenv("secrets.env")
@@ -15,35 +15,21 @@ class Stealthybot(commands.Bot):
 
     def __init__(self):
         super().__init__(
-            command_prefix= "s?test",
+            command_prefix= "s?",
             description="worst bot ever lol",
-            intents = discord.Intents.all(),
+            intents = disnake.Intents.all(),
             case_insensitive= True,
             owner_ids=[521226389559443461]
         )
         
-    
         for filename in os.listdir("./cogs/"):
             if filename.endswith('.py') and not filename.startswith('dutils'):
                     self.load_extension(f'cogs.{filename[:-3]}')
-
-    async def elmayo(self,message:str, channel:int, name:str ):
-        """this sends a message to a specified channel
-
-        Args:
-            message (str): the message that needs to be send
-            channel (int): the id of the channel
-            name (str): the name of the embed
-        """    
-        embed = discord.Embed(title=message, description="")
-        embed.set_author(name=name,icon_url="https://cdn.slangit.com/img/sc/twitch/smile.png")
-        await self.get_channel(channel).send(embed = embed)
  
-
     async def on_ready(self):
         print(f"We have logged in as {self.user}")
 
-        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name =f"{self.command_prefix}help"))
-        print(discord.__version__)
+        await self.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching, name =f"{self.command_prefix}help"))
+        print(disnake.__version__)
 
 
