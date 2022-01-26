24f3efc1b7f229437e5c00b9ad8be871277958a3
stealthninja <realstealthninja@gmail.com>
2022-01-26 11:12:27 +0530

    small changes

--- cogs/gaming.py
+++ cogs/gaming.py
@@ -8,7 +8,6 @@ from coc import utils
 from dotenv import load_dotenv
 from disnake.ext import commands
 
-import utils.images as images
 from utils.dutils import paginate
 
 
@@ -35,7 +34,7 @@ class Gaming(commands.Cog):
         self.bot = bot
         
     @commands.command(description="gets info about a player using their username accepted platforms are: psn, xbl, orgin")
-    async def getplayer(self, ctx, username, platform="orgin"):
+    async def apexprofile(self, ctx, username, platform="orgin"):
         ctx.trigger_typing
         r= requests.get(f"https://public-api.tracker.gg/v2/apex/standard/profile/{platform}/{username}", headers={"TRN-Api-Key": f"{apitoken}", "Accept": "application/json", "Accept-Encoding":"gzip"})
         respcode = r.status_code
@@ -111,62 +110,58 @@ class Gaming(commands.Cog):
             listofem = [embed, emby,em]
             
             await paginate(ctx=ctx,embeds=listofem)
+        
     logging.basicConfig(level=logging.ERROR)
     @commands.command(description="gets the clan detailed about the specified clan")
-    async def claninfo(self, ctx, clan_tag, ):
-        ctx.trigger_typing
+    async def clanprofile(self, ctx, clan_tag, ):
+        await ctx.trigger_typing()
         if not utils.is_valid_tag(clan_tag):
-
             await ctx.send("You didn't give me a proper tag!")
             return
-
-
+    
         try:
-
-            clan = await self.coc_client.get_clan(clan_tag)
-
+            clan = await self.bot.coc_client.get_clan(clan_tag)
+        
         except coc.NotFound:
-
             await ctx.send("This clan doesn't exist!")
-
             return
-
-
-
-        if clan.public_war_log is False:
-
-            log = "Private"
-
-        else:
-
-            log = "Public"
 
 
         e = disnake.Embed(title="Clan details")
-
         e.set_thumbnail(url=clan.badge.url)
-        
-        e.add_field(name="Clan Name", value=f"{clan.name}({clan.tag})\n[Open in game]({clan.share_link})", inline=False)
-        e.add_field(name="Description", value=clan.description, inline=False)
-        e.add_field(name="Leader", value=clan.get_member_by(role=coc.Role.leader))
-        e.add_field(name="Clan Type", value=clan.type)
-        e.add_field(name="Location", value=clan.location)
-        e.add_field(name="Total Clan Trophies", value=f"{clan.points} <:Trophy:896983724686737439>")
-        e.add_field(name="Total Clan Versus Trophies", value=f"{clan.versus_points}  <:Icon_Versus_Trophy:896979379094184016>")
-        e.add_field(name="Required Trophies", value=f" {clan.required_trophies} <:Trophy:896983724686737439>" , inline=False)
-        e.add_field(name="War Win Streak", value=clan.war_win_streak, inline=False)
-        e.add_field(name="War Frequency", value=clan.war_frequency, inline=False)
-        e.add_field(name="Clan War League Rank", value=clan.war_league, inline=False)
-        e.add_field(name="Clan Labels", value="\n".join(f"> {label.name}" for label in clan.labels), inline=False)
-        e.add_field(name="Member Count", value=f"> {clan.member_count}/50", inline=False)
+        coleaders= clan.get_member_by(role=coc.Role.co_leader)
+        labels =  "\n".join(f"> {label.name}" for label in clan.labels)
+        e.add_field(
+            name=f"**{clan.name} {clan.tag}**   | {clan.type}",
+            value=f"""
+               `{clan.description}`
+            > **location**: {clan.location}
+            
+            > **Total players**: **`{clan.member_count}/50`**
+            
+            > **Join requirement**: `{clan.required_trophies} <:Trophy:896983724686737439>`
+            > **Trophies**: `{clan.points} <:Trophy:896983724686737439>`
+            > **Verses Trophies**: `{clan.points} <:Trophy:896983724686737439>`
+            
+            > **Leader**: `{clan.get_member_by(role=coc.Role.leader)}`
+            > **Co-Leader(s)**:
+            > `{coleaders}`
+
+            **labels**: 
+            {labels}
+            
+            > **Win Streak**: `{clan.war_win_streak}`
+            > **League war rank**: `{clan.war_league}` 
+
+            """
+        )
         e.add_field(
 
             name="Clan Record",
 
-            value=f"> Won - {clan.war_wins}\n> Lost - {clan.war_losses}\n> Draw - {clan.war_ties}",
+            value=f"> Wins - `{clan.war_wins}`\n> Losses - `{clan.war_losses}`\n> Draws - `{clan.war_ties}`",
 
             inline=False
-
         )
         e2= ""
         async for player in clan.get_detailed_members():
@@ -174,37 +169,5 @@ class Gaming(commands.Cog):
         e.add_field(name="Members", value=e2)
         await ctx.send(embed=e)
       
-    @commands.command()
-    async def playerheroes(self,ctx, player_tag):
-        if not utils.is_valid_tag(player_tag):
-           await ctx.send("You didn't give me a proper tag!")
-           return
-        try:
-           player = await self.bot.coc_client.get_player(player_tag)
-        except coc.NotFound:
-           await ctx.send("This player doesn't exist!")
-           return
-    
-        to_send = ""
-        listofimages =[]
-        if len(player.heroes) == 1:
-           embed = disnake.Embed(title="Player heros")
-           embed.set_image(url=f"attachment://assets/heros/{player.heroes[0]}.png")
-           embed.add_field(name=str(player.heroes[0]), value="Lv{}/{}".format(player.heroes[0].level,player.heroes[0].max_level))
-           await ctx.send(embed=embed)
-        
-        else:
-            for hero in player.heroes:
-                listofimages.append(f"assets/heros/{str(hero)}.png")
-                to_send += "{}: Lv{}/{}   ".format(str(hero), hero.level, hero.max_level)
-            imgpath = images.stichimgs(listofimages,"herostiched")
-            embed = disnake.Embed(title="Player Heroes")
-            embed.set_image(url=f"attachment://{imgpath}")
-            embed.add_field(name=to_send,value="⠀")
-            
-            file = disnake.File("assets/stichedimages/herostiched.png")
-            await ctx.send(file=file,embed=embed)
-            os.remove(imgpath)    
-
 def setup(bot):
     bot.add_cog(Gaming(bot))
