3c706c4de5a5f20784a0c6e6f0bae673cb9504e8
realstealthninja <realstealthninja@gmail.com>
2023-09-28 20:26:05 +0530

    fix: add check for if messgae was in dm

--- cogs/activity/cog.py
+++ cogs/activity/cog.py
@@ -22,21 +22,19 @@ NAMES = [
     "🗣",
     "🗣general",
     "chat",
-    "main-chat"
+    "main-chat",
 ]
 
 
 class Activity(commands.Cog, name="activity"):
-    """ Main cog for all the activity commands and detection"""
+    """Main cog for all the activity commands and detection"""
 
     def __init__(self, bot):
         self.bot = bot
         self.database = self.bot.act_database
         self.act_helper = ActivityHelper(self.bot)
         self.cooldown = commands.CooldownMapping.from_cooldown(
-            5.0,
-            10.0,
-            commands.BucketType.user
+            5.0, 10.0, commands.BucketType.user
         )
         self.reset.start()
 
@@ -58,52 +56,64 @@ class Activity(commands.Cog, name="activity"):
                 for user in userlist:
                     user = await guild.getch_member(user[1])
                     if not user:
-                         await self.act_helper.delete(
+                        await self.act_helper.delete(
                             "Users",
-                            filter_columns=["ServerID", "UserID",],
-                            filter_values=(userlist[0][0], userlist[0][1]))
-                leaderboard_text = "`Sl.|                  Name                  | Msg `\n```\n"
-                leaderboard_text = await self.leaderboard_maker(guild, 3, leaderboard_text)
-                embed = disnake.Embed(
-                ).add_field(
-                    name="⠀­",
-                    value=f"{leaderboard_text}```"
+                            filter_columns=[
+                                "ServerID",
+                                "UserID",
+                            ],
+                            filter_values=(userlist[0][0], userlist[0][1]),
+                        )
+                leaderboard_text = (
+                    "`Sl.|                  Name                  | Msg `\n```\n"
+                )
+                leaderboard_text = await self.leaderboard_maker(
+                    guild, 3, leaderboard_text
+                )
+                embed = disnake.Embed().add_field(
+                    name="⠀­", value=f"{leaderboard_text}```"
                 )
 
                 await self.act_helper.update_set(
                     "Users",
-                    column=["MsgCount", ],
-                    values=(0,)
+                    column=[
+                        "MsgCount",
+                    ],
+                    values=(0,),
                 )
 
                 for k in guild.text_channels:
                     if k.name in NAMES or "general" in k.name or "chat" in k.name:
                         await k.send(embed=embed)
-                        return await k.send(embed=disnake.Embed(
-                            title="The leaderboard has been reset",
-                            description="Thank you for being active"
-                        ))
+                        return await k.send(
+                            embed=disnake.Embed(
+                                title="The leaderboard has been reset",
+                                description="Thank you for being active",
+                            )
+                        )
 
     @commands.command()
     async def activity(self, ctx: commands.Context, member: disnake.Member = None):
         """
         Activity rank command
         """
-        
+
         if not await self.act_helper.fetch_server(ctx.guild.id):
             return
-        
+
         if not member:
             member = ctx.guild.get_member(ctx.author.id)
-        
+
         await self.act_helper.create_or_fetch_user(member.id, ctx.guild.id)
         image = await self.act_helper.create_act_image(member)
 
-        await ctx.send(file=disnake.File(
-            fp=image,
-            filename="Activity.png",
-            description="You know its bad when you have activity points"
-        ))
+        await ctx.send(
+            file=disnake.File(
+                fp=image,
+                filename="Activity.png",
+                description="You know its bad when you have activity points",
+            )
+        )
 
     @reset.before_loop
     async def before_reset(self):
@@ -123,110 +133,64 @@ class Activity(commands.Cog, name="activity"):
         """
         # main anti spam
         retry_after = self.cooldown.get_bucket(message).update_rate_limit()
-        server = await self.act_helper.fetch_server(
-            message.guild.id
-        )
+
+        if not message.guild:
+            return
+
+        server = await self.act_helper.fetch_server(message.guild.id)
 
         if not server or message.author.bot or retry_after:
             return
 
         user = await self.act_helper.create_or_fetch_user(
-            message.author.id,
-            message.guild.id
+            message.author.id, message.guild.id
         )
 
         await self.act_helper.update_set(
             "Users",
-            column=[
-                "MsgCount",
-                "TotalMsgCount"
-            ],
-            values=(
-                user[3] + 1,
-                user[4] + 1
-            ),
-            filter_columns=[
-                "ServerID",
-                "UserID"
-            ],
-            filter_values=(
-                user[0],
-                user[1]
-            )
+            column=["MsgCount", "TotalMsgCount"],
+            values=(user[3] + 1, user[4] + 1),
+            filter_columns=["ServerID", "UserID"],
+            filter_values=(user[0], user[1]),
         )
 
         if user[3] + 1 == server[1]:
             await self.act_helper.update_set(
                 "Users",
-                column=[
-                    "ActScore"
-                ],
-                values=(
-                    user[2] + 1,
-                ),
-                filter_columns=[
-                    "ServerID",
-                    "UserID"
-                ],
-                filter_values=(
-                    user[0],
-                    user[1]
-                )
+                column=["ActScore"],
+                values=(user[2] + 1,),
+                filter_columns=["ServerID", "UserID"],
+                filter_values=(user[0], user[1]),
             )
             msg = await self.act_helper.fetch_message(server[0])
             embed = await self.act_helper.fetch_embed(server[0])
             msg = msg[0]
             if msg:
                 await message.channel.send(
-                    msg.replace(
-                        "MENTION_USER",
-                        f"{message.author.mention}"
-                    ).replace(
-                        "MSG_COUNT",
-                        f"{user[3]}"
-                    ).replace(
-                        "TOTAL_MSG_COUNT",
-                        f"{user[4]}"
-                    ).replace(
-                        "MSG_GOAL",
-                        f"{server[1]}"
-                    ).replace(
-                        "USERNAME",
-                        f"{message.author.display_name}"
-                    )
+                    msg.replace("MENTION_USER", f"{message.author.mention}")
+                    .replace("MSG_COUNT", f"{user[3]}")
+                    .replace("TOTAL_MSG_COUNT", f"{user[4]}")
+                    .replace("MSG_GOAL", f"{server[1]}")
+                    .replace("USERNAME", f"{message.author.display_name}")
                 )
             if embed:
                 embed.title.replace(
-                    "MENTION_USER",
-                    f"{message.author.mention}"
+                    "MENTION_USER", f"{message.author.mention}"
+                ).replace("MSG_COUNT", f"{user[3]}").replace(
+                    "TOTAL_MSG_COUNT", f"{user[4]}"
                 ).replace(
-                    "MSG_COUNT",
-                    f"{user[3]}"
+                    "MSG_GOAL", f"{server[1]}"
                 ).replace(
-                    "TOTAL_MSG_COUNT",
-                    f"{user[4]}"
-                ).replace(
-                    "MSG_GOAL",
-                    f"{server[1]}"
-                ).replace(
-                    "USERNAME",
-                    f"{message.author.display_name}"
+                    "USERNAME", f"{message.author.display_name}"
                 )
                 embed.description.replace(
-                    "MENTION_USER",
-                    f"{message.author.mention}"
+                    "MENTION_USER", f"{message.author.mention}"
+                ).replace("MSG_COUNT", f"{user[3]}").replace(
+                    "TOTAL_MSG_COUNT", f"{user[4]}"
                 ).replace(
-                    "MSG_COUNT",
-                    f"{user[3]}"
+                    "MSG_GOAL", f"{server[1]}"
                 ).replace(
-                    "TOTAL_MSG_COUNT",
-                    f"{user[4]}"
-                ).replace(
-                    "MSG_GOAL",
-                    f"{server[1]}"
-                ).replace(
-                    "USERNAME",
-                    f"{message.author.display_name}"
+                    "USERNAME", f"{message.author.display_name}"
                 )
                 await message.channel.send(embed=embed)
 
@@ -243,9 +207,9 @@ class Activity(commands.Cog, name="activity"):
                 title="Welcome to the setup!",
                 description="""
                 press the `confirm` button to continue with the setup!
-                """
+                """,
             ),
-            view=SetupActivity(ctx, self.act_helper)
+            view=SetupActivity(ctx, self.act_helper),
         )
 
     @commands.group(name="leaderboard", aliases=["lb", "board"], pass_context=True)
@@ -272,11 +236,7 @@ class Activity(commands.Cog, name="activity"):
 
         leaderboard_text = "`Sl.|                  Name                  | Msg `\n```\n"
         leaderboard_text = await self.leaderboard_maker(ctx.guild, 3, leaderboard_text)
-        embed = disnake.Embed(
-        ).add_field(
-            name="⠀­",
-            value=f"{leaderboard_text}```"
-        )
+        embed = disnake.Embed().add_field(name="⠀­", value=f"{leaderboard_text}```")
         await ctx.send(embed=embed)
 
     @leaderboard.command(name="act")
@@ -288,11 +248,7 @@ class Activity(commands.Cog, name="activity"):
 
         leaderboard_text = "`Sl.|                  Name                  | ACT `\n```\n"
         leaderboard_text = await self.leaderboard_maker(ctx.guild, 2, leaderboard_text)
-        embed = disnake.Embed(
-        ).add_field(
-            name="⠀­",
-            value=f"{leaderboard_text}```"
-        )
+        embed = disnake.Embed().add_field(name="⠀­", value=f"{leaderboard_text}```")
         await ctx.send(embed=embed)
 
     @leaderboard.command(name="total")
@@ -304,18 +260,10 @@ class Activity(commands.Cog, name="activity"):
 
         leaderboard_text = "`Sl.|                  Name                  | Msg `\n```\n"
         leaderboard_text = await self.leaderboard_maker(ctx.guild, 4, leaderboard_text)
-        embed = disnake.Embed(
-        ).add_field(
-            name="⠀­",
-            value=f"{leaderboard_text}```"
-        )
+        embed = disnake.Embed().add_field(name="⠀­", value=f"{leaderboard_text}```")
         await ctx.send(embed=embed)
 
-    async def check_user_pop(
-        self,
-        guild: disnake.Guild,
-        userlist: list
-    ) -> list:
+    async def check_user_pop(self, guild: disnake.Guild, userlist: list) -> list:
         """
         checks the list of users then pops the ones that have left the guild or dont exist
         """
@@ -326,10 +274,8 @@ class Activity(commands.Cog, name="activity"):
         return userlist
 
     async def leaderboard_maker(
-            self,
-            guild: disnake.Guild,
-            index: int,
-            toptext: str) -> str:
+        self, guild: disnake.Guild, index: int, toptext: str
+    ) -> str:
         """
         Creates a leaderboard based on the parameter given
 
@@ -344,17 +290,17 @@ class Activity(commands.Cog, name="activity"):
         users.sort(key=lambda i: i[index], reverse=True)
         if len(users) > 10:
             while len(users) != 10:
-                users.pop(len(users)-1)
+                users.pop(len(users) - 1)
 
         for count, user in enumerate(users):
-            if count+1 == 11:
+            if count + 1 == 11:
                 break
             name = await guild.getch_member(user[1])
             name = name.display_name
             msg_count = str(user[index])
             if len(name) != 32:
-                name += "".join(" " for num in range(32-len(name)))
-            toptext += str(count+1) + " | " + name + " | " + msg_count + "\n"
+                name += "".join(" " for num in range(32 - len(name)))
+            toptext += str(count + 1) + " | " + name + " | " + msg_count + "\n"
         return toptext
 
 
